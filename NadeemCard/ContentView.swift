8e947ebef3601c217e7a2ffe9cdb03404dbde07d
Nadeem Ansari <nadeem_ansari64@outlook.com>
2020-06-13 21:52:15 -0400

    Initial Commit

---
+++ NadeemCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NadeemCard
+//
+//  Created by Nadeem Ansari on 6/13/20.
+//  Copyright © 2020 Nadeem Ansari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
